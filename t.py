34a984eaadc33470c18b2ec6b66a4e77eda5a6a2
526awd <a1145aq@126.com>
2025-01-04 11:50:40 +0800

    Update t.py

--- t.py
+++ t.py
@@ -1,2 +1,17 @@
 imoort os
-os.system('')
+os.system('cp Zundamon「なのだ」1735928147797.mp4 1.mp4')
+os.mkdir('morr')
+os.system('cp 1.mp4 morr/1.mp4')
+
+for i in range(400):
+    os.system('ffmpeg -f concat -safe 0 -i input.txt -c copy output.mp4')
+    os.system('rm -f 1.mp4')
+    print(i)
+    os.system('move output.mp4 1.mp4')
+os.system('rm -f morr/1.mp4')
+os.system('cp 1.mp4 morr/1.mp4')
+for i in range(216):
+    os.system('ffmpeg -f concat -safe 0 -i input.txt -c copy output.mp4')
+    os.system('rm -f 1.mp4')
+    os.system('move output.mp4 1.mp4')
+    print(i)
