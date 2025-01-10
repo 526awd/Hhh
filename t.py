71dbd11b0b47fc91f401d6977e095d476a0d2c87
526awd <a1145aq@126.com>
2025-01-11 00:02:56 +0800

    Update t.py

--- t.py
+++ t.py
@@ -8,10 +8,10 @@ for i in range(400):
     os.system('rm -f 1.mp4')
     print(i)
     os.system('mv output.mp4 1.mp4')
-os.system('rm -f morr/1.mp4')
-os.system('cp 1.mp4 morr/1.mp4')
-for i in range(216):
-    os.system('ffmpeg -f concat -safe 0 -i input.txt -c copy output.mp4')
-    os.system('rm -f 1.mp4')
-    os.system('mv output.mp4 1.mp4')
-    print(i)
+#os.system('rm -f morr/1.mp4')
+#os.system('cp 1.mp4 morr/1.mp4')
+#for i in range(216):
+#    os.system('ffmpeg -f concat -safe 0 -i input.txt -c copy output.mp4')
+#    os.system('rm -f 1.mp4')
+#    os.system('mv output.mp4 1.mp4')
+#    print(i)
