717b78783d31bc0958956e0557aed78ce54d63b6
526awd <a1145aq@126.com>
2025-01-16 02:04:46 +0800

    Update download_illusts.py

--- download_illusts.py
+++ download_illusts.py
@@ -31,29 +31,16 @@ def main():
         os.makedirs(directory)
 
     # download top3 day rankings to 'illusts' dir
-    for idx, illust in enumerate(json_result.illusts[:4]):
+    for idx, illust in enumerate(json_result.illusts):
         image_url = illust.meta_single_page.get(
             "original_image_url", illust.image_urls.large
         )
         print("{}: {}".format(illust.title, image_url))
 
         # try four args in MR#102
-        if idx == 0:
-            api.download(image_url, path=directory, name=None)
-        elif idx == 1:
-            url_basename = os.path.basename(image_url)
-            extension = os.path.splitext(url_basename)[1]
-            name = "illust_id_%d_%s%s" % (illust.id, illust.title, extension)
-            api.download(image_url, path=directory, name=name)
-        elif idx == 2:
-            api.download(image_url, path=directory, fname="illust_%s.jpg" % (illust.id))
-        else:
-            # path will not work due to fname is a handler
-            api.download(
-                image_url,
-                path="directory",
-                fname=open("{}/illust_{}.jpg".format(directory, illust.id), "wb"),
-            )
+        
+        api.download(image_url, path=directory, name=illust.title)
+        
 
 
 if __name__ == "__main__":
